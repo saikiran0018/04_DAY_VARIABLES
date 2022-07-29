5ad654a88a96cfb9de4d6f86f050db5ea90a1f70
saikiran0018 <109685878+saikiran0018@users.noreply.github.com>
2022-07-29 12:26:59 +0530

    Add files via upload

---
+++ veriables.py
@@ -0,0 +1,55 @@
+a=5
+b=6
+c=7
+print(a)                      #5
+print(b)                      #6
+print(c)                      #7
+
+x=1 ; y=3; z=6
+print(x) ; print(y); print(z)
+print(x,y,z)                  #1 3 6
+
+
+x=7
+print(x)                      #7
+p=5
+x=3
+print(x,p)                    #3 5
+
+
+
+x=7
+p=5
+x=3
+print(x,p)                      #3 
+
+
+
+p=5
+x=3
+print(z)                        #nameerorr: name "z" is not defind
+
+
+x,y=1,2,3
+print(z)                          #valueerror
+
+
+x,y,z=1,2                           #valueerror: not enough value
+
+
+a=1,2,3,4
+print(a)                            #1,2,3,4
+print(type(a))                      #tuple
+
+
+
+a=1,2,3,4
+print(a,type(a))                     #1,2,3,4      #tuple
+
+
+print('hi'+'bye')                      #hibye
+print('hi'+5)                          #type erorr
+
+
+
+
